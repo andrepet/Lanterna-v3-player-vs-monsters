31b948d0b8c47a9dd6bc629eb1cde3f8a9e3911e
andrepet <pettersson.andreas.lennart@gmail.com>
2018-11-02 10:35:48 +0100

    Remove old import

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
